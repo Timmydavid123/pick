435b97ae6a198f06052f8ddbf3ce95f354e6f2d0
Timmydavid123 <adeleketimileyin11>
2024-12-09 14:56:42 +0100

    hi

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: 'https://pick-six.vercel.app', credentials: true }));
 app.use(bodyParser.json());
 
 // MongoDB connection
