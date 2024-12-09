8c256e8a95e36f8b42c5362fc8f71ccf0acd5181
Timmydavid123 <adeleketimileyin11>
2024-12-09 22:17:03 +0100

    kju

--- client/src/WishlistForm.js
+++ client/src/WishlistForm.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import Swal from 'sweetalert2';
 import { AuthContext } from './AuthContext';
 
